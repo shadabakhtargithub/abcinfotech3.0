d9c1d107a4ed5e230646012e4c2e1291c2dd902e
Shadabdev1 <shadabakhtar97@gmail.com>
2021-10-31 06:15:50 +0000

    files added by shadabdev1

---
+++ file1.java
@@ -0,0 +1,10 @@
+// This Simple Java Project with Single File
+
+public class file1
+ {
+	 public static void main(String args[])
+	 {
+		 System.out.println("This is Java Project with 1 File");
+	}
+}
+
